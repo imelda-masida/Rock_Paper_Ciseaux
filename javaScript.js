dc57c76b518bdddc1c5a4e06f8ee4b0150afac34
stone <imeldamasidakakudji@gmail.com>
2026-05-30 11:27:33 +0200

    add function getHumainChoice

--- javaScript.js
+++ javaScript.js
@@ -1,7 +1,15 @@
-console.log("Hello, World!");
+
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let textHumain = prompt("Please enter rock, paper or scissors:");
+    textHumain = textHumain.toLowerCase();
+    return textHumain;
+}
+console.log(getHumanChoice());
