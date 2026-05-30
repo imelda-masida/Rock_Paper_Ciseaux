59090cc2a15e54b62e550b496380958917e259ea
stone <imeldamasidakakudji@gmail.com>
2026-05-30 12:18:25 +0200

    add fuction/ tests et fin

--- javaScript.js
+++ javaScript.js
@@ -8,11 +8,37 @@ function getComputerChoice() {
 console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let textHumain = prompt("Please enter rock, paper or scissors:");
-    textHumain = textHumain.toLowerCase();
-    return textHumain;
+    let textHuman = prompt("Please enter rock, paper or scissors:");
+    textHuman = textHuman.toLowerCase();
+    return textHuman;
 }
 console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    console.log(`You chose: ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+    
+}
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+playRound(humanSelection, computerSelection);
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const computerSelection = getComputerChoice();
+        const humanSelection = getHumanChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    if (humanScore > computerScore) {
+        console.log("Congratulations! You win the game!");
+    } else if (computerScore > humanScore) {
+        console.log("Sorry! The computer wins the game!");
+    } else {
+        console.log("It's a tie!");
+    }
+}
+playGame();
